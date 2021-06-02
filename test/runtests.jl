872bf9ce62b40e0d578e15eff4161098cd4b41c5
hyrodium <hyrodium@gmail.com>
2021-06-02 23:07:03 +0900

    add first test

---
+++ test/runtests.jl
@@ -0,0 +1,12 @@
+using ImageClipboard
+using Images
+using Test
+
+@testset "ImageClipboard.jl" begin
+    @testset "RGB image copy & paste" begin
+        img = rand(RGB{N0f8}, 100, 200)
+        clipboard_img(img)
+        img2 = clipboard_img()
+        @test img == img2
+    end
+end
