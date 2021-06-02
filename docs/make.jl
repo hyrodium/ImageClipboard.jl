c10463868988a2db67343229fe141f5b637532fe
hyrodium <hyrodium@gmail.com>
2021-06-02 23:14:47 +0900

    add docs (fix #2)

---
+++ docs/make.jl
@@ -0,0 +1,23 @@
+using ImageClipboard
+using Documenter
+
+DocMeta.setdocmeta!(ImageClipboard, :DocTestSetup, :(using ImageClipboard); recursive=true)
+
+makedocs(;
+    modules=[ImageClipboard],
+    authors="hyrodium <hyrodium@gmail.com> and contributors",
+    repo="https://github.com/hyrodium/ImageClipboard.jl/blob/{commit}{path}#{line}",
+    sitename="ImageClipboard.jl",
+    format=Documenter.HTML(;
+        prettyurls=get(ENV, "CI", "false") == "true",
+        canonical="https://hyrodium.github.io/ImageClipboard.jl",
+        assets=String[],
+    ),
+    pages=[
+        "Home" => "index.md",
+    ],
+)
+
+deploydocs(;
+    repo="github.com/hyrodium/ImageClipboard.jl",
+)
---
+++ docs/src/index.md
@@ -0,0 +1,14 @@
+```@meta
+CurrentModule = ImageClipboard
+```
+
+# ImageClipboard
+
+Documentation for [ImageClipboard](https://github.com/hyrodium/ImageClipboard.jl).
+
+```@index
+```
+
+```@autodocs
+Modules = [ImageClipboard]
+```
