3c788e2546cfe643c9fe1231fda7b0d752f822c9
hyrodium <hyrodium@gmail.com>
2021-06-02 23:08:05 +0900

    add more test for RGBA

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,10 +3,19 @@ using Images
 using Test
 
 @testset "ImageClipboard.jl" begin
+
     @testset "RGB image copy & paste" begin
         img = rand(RGB{N0f8}, 100, 200)
         clipboard_img(img)
         img2 = clipboard_img()
         @test img == img2
     end
+
+    @testset "RGBA image copy & paste" begin
+        img = rand(RGBA{N0f8}, 100, 200)
+        clipboard_img(img)
+        img2 = clipboard_img()
+        @test img == img2
+    end
+
 end
