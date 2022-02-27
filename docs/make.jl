e278b73ed6b5886ca626239e4732fac00dde9a7d
hyrodium <hyrodium@gmail.com>
2022-02-27 18:53:04 +0900

    update make.jl for favicon

--- docs/make.jl
+++ docs/make.jl
@@ -11,7 +11,7 @@ makedocs(;
     format=Documenter.HTML(;
         prettyurls=get(ENV, "CI", "false") == "true",
         canonical="https://hyrodium.github.io/ImageClipboard.jl",
-        assets=String[],
+        assets=["assets/favicon.ico"],
     ),
     pages=[
         "Home" => "index.md",
